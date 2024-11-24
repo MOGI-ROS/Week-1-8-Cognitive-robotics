4f02670540b8c277814ae53ce83c0ad18139a262
David Dudas <david.dudas@outlook.com>
2024-11-24 10:58:13 +0100

    add package licencing

--- turtlebot3_mogi_py/setup.py
+++ turtlebot3_mogi_py/setup.py
@@ -4,7 +4,7 @@ package_name = 'turtlebot3_mogi_py'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='david',
+    maintainer='David Dudas',
     maintainer_email='david.dudas@outlook.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='BME MOGI ROS course related python nodes to the official Turtlebot 3 packages',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
